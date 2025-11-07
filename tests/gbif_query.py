cb9fe70ea06552a5226f0aa040a44a1fa7d6ac01
etienengodin <manat@DESKTOP-STSFAGO.localdomain>
2025-11-07 13:22:58 -0500

    gbif query loading .env

--- tests/gbif_query.py
+++ tests/gbif_query.py
@@ -1,10 +1,9 @@
 from Cscorer.data.query import get_data
+from Cscorer.core import read_config
+from pathlib import Path
 
-
-config = { "BOUNDS":[-73.591232,-73.583851,45.513204,45.517895],
-"TAXON_KEY": 7707728
-}
-
+path = Path(__file__).parent / "gbif_query.yaml"
+config = read_config(path)
 query = get_data('gbif', config)
-
 print(query.predicate)
+query.run()
