030437b7d254dbcb5d2dba3994c5112711458f20
etienengodin <manat@DESKTOP-STSFAGO.localdomain>
2025-11-08 19:17:19 -0500

    adding duckdb utils from previous project, adding duckdb connection to PipelineData

--- src/Cscorer/main.py
+++ src/Cscorer/main.py
@@ -2,6 +2,7 @@ from .data.factory import create_query
 from .core import read_config, write_config, PipelineData, StepStatus
 from .data.main import get_data
 from .utils.debug import launch_debugger
+from .utils.duckdb import _open_connection
 from pathlib import Path
 import argparse
 import subprocess
@@ -28,7 +29,7 @@ def main():
     if args.debug:
         launch_debugger()
     
-    
+
     if not args.file:
         if not args.dev:
             raise UserWarning("Missing config file")
@@ -43,10 +44,13 @@ def main():
     run_folder = Path(config_path).parent
     data_folder = (run_folder / 'data')
     pipe_folder = (data_folder / 'pipeline')
+    db_path = data_folder / 'data.duckdb'
     
     if args.force:
         if (config_path.parent / 'data').exists():
-        shutil.rmtree(str(config_path.parent / 'data'))
+            shutil.rmtree(str(config_path.parent / 'data'))
+    
+    
     
     data_folder.mkdir(exist_ok= True)
     pipe_folder.mkdir(exist_ok= True)
@@ -58,18 +62,24 @@ def main():
     #New instance if totally new run 
     if not (pipe_folder / 'pipe_data.yaml').exists():
         logging.info("No pipe data found, creating new instance from scratch")
-        data = PipelineData(config = config)
+        data = PipelineData(config = config,
+                            db_con= _open_connection(db_path)
+)
     else:
         #Read from disk 
         try:
-            data = (PipelineData(config= config, storage = read_config(pipe_folder / 'pipe_data.yaml'), step_status= read_config(pipe_folder / 'pipe_steps.yaml')))
+            data = (PipelineData(config= config,
+                                 storage = read_config(pipe_folder / 'pipe_data.yaml'),
+                                 step_status= read_config(pipe_folder / 'pipe_steps.yaml'),
+                                 db_con= _open_connection(db_path)
+                                 
+                                 ))
             logging.info("Previous pipe data found, creating new instance from data on disk")
 
         except Exception as e:
             raise Exception(e)
 
             
-    logging.info('Running')
     asyncio.run(get_data(data))
 
     
