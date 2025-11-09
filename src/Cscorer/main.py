1f4277b8c02dfa27aa6360ab8f60c17678f58f9f
etienengodin <manat@DESKTOP-STSFAGO.localdomain>
2025-11-08 19:53:06 -0500

    better handling of duckdb connection

--- src/Cscorer/main.py
+++ src/Cscorer/main.py
@@ -1,8 +1,9 @@
 from .data.factory import create_query
-from .core import read_config, write_config, PipelineData, StepStatus
-from .data.main import get_data
+from .core import read_config, write_config, PipelineData
+from .data.get_data import get_gbif
 from .utils.debug import launch_debugger
 from .utils.duckdb import _open_connection
+
 from pathlib import Path
 import argparse
 import subprocess
@@ -11,6 +12,7 @@ from pprint import pprint
 import logging
 import shutil
 
+    
 def main():
     
     parser = argparse.ArgumentParser(
@@ -26,66 +28,63 @@ def main():
 
     args = parser.parse_args()
     
+    # Debugger
     if args.debug:
         launch_debugger()
     
-
+    # Check if required file, else try dev mode
     if not args.file:
         if not args.dev:
             raise UserWarning("Missing config file")
         #dev branch
         config_path = Path(__file__).parent.parent.parent / "work/dev/config.yaml"
-
     else:
         config_path = args.file
-    
+        
+    #COnfig as dict
     config = read_config(config_path)
     
+    folders = {}
     run_folder = Path(config_path).parent
     data_folder = (run_folder / 'data')
     pipe_folder = (data_folder / 'pipeline')
     db_path = data_folder / 'data.duckdb'
     
+    #Set folder paths to config
+    folders["run_folder"] = str(run_folder)
+    folders["data_folder"] = str(data_folder)
+    folders["pipeline_folder"] = str(pipe_folder)
+    config['folders'] = folders
+    
+    # Force flag to wipe data
     if args.force:
-        if (config_path.parent / 'data').exists():
+        if data_folder.exists():
             shutil.rmtree(str(config_path.parent / 'data'))
-    
-    
-    
-    data_folder.mkdir(exist_ok= True)
-    pipe_folder.mkdir(exist_ok= True)
 
-    config["run_folder"] = str(run_folder)
-    config["data_folder"] = str(data_folder)
-    config["pipeline_folder"] = str(pipe_folder)
-    
-    #New instance if totally new run 
-    if not (pipe_folder / 'pipe_data.yaml').exists():
+    #New instance if totally new run (or forced)
+    if not data_folder.exists():
         logging.info("No pipe data found, creating new instance from scratch")
-        data = PipelineData(config = config,
-                            db_con= _open_connection(db_path)
-)
+        data = PipelineData(config = config)
+        
+        # Create folders 
+        data_folder.mkdir(exist_ok= True)
+        pipe_folder.mkdir(exist_ok= True)
+        
     else:
         #Read from disk 
         try:
             data = (PipelineData(config= config,
                                  storage = read_config(pipe_folder / 'pipe_data.yaml'),
-                                 step_status= read_config(pipe_folder / 'pipe_steps.yaml'),
-                                 db_con= _open_connection(db_path)
-                                 
+                                 step_status= read_config(pipe_folder / 'pipe_steps.yaml')
                                  ))
             logging.info("Previous pipe data found, creating new instance from data on disk")
 
         except Exception as e:
             raise Exception(e)
 
+
             
-    asyncio.run(get_data(data))
-
-    
-    
-    
-
+    asyncio.run(get_gbif(data))
     
     #asyncio.run(get_data(data))
     
