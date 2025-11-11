b97f33aca48d270431ce8b64580bd171b9eb5ec8
etienengodin <manat@DESKTOP-STSFAGO.localdomain>
2025-11-11 07:08:14 -0500

    async issue with pygbif

--- src/Cscorer/data/gbif.py
+++ src/Cscorer/data/gbif.py
@@ -5,6 +5,13 @@ from pygbif import occurrences as occ
 from ..core import PipelineData, StepStatus
 import logging
 from enum import Enum
+import asyncio
+
+
+class GbifClient:
+    #wrapper for gbif api functions ( submit, download, etc,  query as input )
+    pass
+
 
 class GbifQuery(BaseQuery):
 
@@ -28,21 +35,6 @@ class GbifQuery(BaseQuery):
         data.update_step_status(step_name, StepStatus.local)
         
         return gbif_raw_data
-            
-        
-    def _predicate_builder(self,config:dict):
-        predicate = GbifPredicate()
-        for key, value in config.items():
-            if key == 'LIMIT':
-                continue
-            if key == 'GEOMETRY':
-                predicate.add_geometry(value)
-                continue
-            if key == 'YEAR':
-                predicate.add_field(key,value, type = 'greaterThan') # custom for year 
-                continue
-            predicate.add_field(key, value)
-        return predicate
     
     async def _submit_request(self, data:PipelineData):  
         logger = data.logger
@@ -51,23 +43,22 @@ class GbifQuery(BaseQuery):
         load_dotenv(env_path)
         try:
             query = self.predicate.to_dict() # build 
-            response = occ.download(query,
+            #threaded as returns tuples 
+            response = await asyncio.to_thread(occ.download(query,
                     format="SIMPLE_CSV",
                     user= str(os.getenv("GBIF_USERNAME")),
                     pwd=str(os.getenv("GBIF_PASSWORD")),
-                    email=str(os.getenv("GBIF_EMAIL"))
-            )   
-            logging.info(f'Gbif query returned : {response[0]}')
+                    email=str(os.getenv("GBIF_EMAIL"))))
+            
+            logger.info(f"Gbif request made. Key : {response[0]}")
         except Exception as e:
-            logger.error(e)
+            logger.error(f"Error running gbif request: {e}")
             raise RuntimeError(e)
 
         if response:   
             data.set('gbif_download_key', response[0])
-            
-        return response[0]
     
-    async def _poll_gbif_until_ready(self, data: PipelineData, poll_interval: int = 60, timeout_seconds: int = 60*60):
+    async def _poll_gbif_until_ready(self, data: PipelineData, poll_interval: int = 30, timeout_seconds: int = 60*60):
         
         """
         Poll GBIF for the download status. Uses occ.download_meta or occ.download_get to inspect.
@@ -80,7 +71,7 @@ class GbifQuery(BaseQuery):
         start = time.time()
 
         while True:
-            meta = occ.download_meta(key=download_key)
+            meta = await occ.download_meta(key=download_key)
             data.logger.debug(meta)
             status = meta.get("status")
             logger.info(f"GBIF status for {download_key}: {status}")
@@ -105,7 +96,7 @@ class GbifQuery(BaseQuery):
         os.makedirs(dest_dir, exist_ok=True)
         logger.info(f"Downloading GBIF data for {download_key} to {dest_dir}...")
         # This returns file path or bytes depending on client; example below writes to path
-        response = occ.download_get(download_key, path=dest_dir)
+        response = await occ.download_get(download_key, path=dest_dir)
         zip_path = response['path']# implementation detail from pygbif
         logger.info(f"Downloaded to {zip_path}")
         # Unpack (if needed) - example uses unzip via python
@@ -120,8 +111,25 @@ class GbifQuery(BaseQuery):
         data.set('gbif_raw_data', output)
         os.remove(zip_path)
         return output
-
+    
+    
+    def _predicate_builder(self,config:dict):
+        predicate = GbifPredicate()
+        for key, value in config.items():
+            if key == 'LIMIT':
+                continue
+            if key == 'GEOMETRY':
+                predicate.add_geometry(value)
+                continue
+            if key == 'YEAR':
+                predicate.add_field(key,value, type = 'greaterThan') # custom for year 
+                continue
+            predicate.add_field(key, value)
+        return predicate
+    
+    
 class GbifPredicate():
+    
     def __init__(self, type = 'and'):
         if type not in ('and', 'or'):
             raise ValueError("Mode must be 'and' or 'or'")
@@ -131,7 +139,7 @@ class GbifPredicate():
     def add_field(self, key :str, value:str, type :str = 'equals'):
         if isinstance(value, list):
             # Requires type to be "in" for list 
-            expr = {"type": 'in', "key": key, "value":value}
+            expr = {"type": 'in', "key": key, "values":value}
         else:
             expr = {"type": type, "key": key, "value":value}
 
