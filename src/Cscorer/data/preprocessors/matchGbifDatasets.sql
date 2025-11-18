8eb97f13bcf49ef4a00f15f723ade615aa9158a3
etienengodin <manat@DESKTOP-STSFAGO.localdomain>
2025-11-18 15:18:16 -0500

    matching datasets per distance and month

--- src/Cscorer/data/preprocessors/matchGbifDatasets.sql
+++ src/Cscorer/data/preprocessors/matchGbifDatasets.sql
@@ -1,27 +1,29 @@
-CREATE INDEX idx_c_geom ON preprocessed.gbif_citizen USING RTREE (geom_c);
-CREATE INDEX idx_e_geom ON preprocessed.gbif_expert USING RTREE (geom_e);
+CREATE INDEX idx_c_geom ON preprocessed.gbif_citizen USING RTREE (geom);
+CREATE INDEX idx_e_geom ON preprocessed.gbif_expert USING RTREE (geom);
+
+CREATE OR REPLACE TABLE preprocessed.matchedGbif AS
 
 WITH c3857 AS(
     
-    SELECT gbifID, ST_Transform(geom_c, 3857) AS geom
+    SELECT gbifID, "month", ST_Transform(geom, 'EPSG:4326', 'EPSG:3857') AS geom
     FROM preprocessed.gbif_citizen
 ),
 
-WITH e3857 AS(
+e3857 AS(
     
-    SELECT gbifID, ST_Transform(geom_e, 3857) AS geom
+    SELECT gbifID, "month", ST_Transform(geom, 'EPSG:4326', 'EPSG:3857') AS geom
     FROM preprocessed.gbif_expert
 )
 
-CREATE OR REPLACE TABLE preprocessed.matchedGbif AS
-SELECT e.gbifID as e_id,
-    c.gbifID as c_id,
+SELECT e.gbifID as e_id, e.month as e_month,
+    c.gbifID as c_id, c.month as c_month
 
 FROM e3857 e
 JOIN c3857 c
     ON ST_DWithin(
-        (SELECT geom_e FROM preprocessed.gbif_expert WHERE gbifID = e.gbifID),  -- index hit
-        (SELECT geom_c FROM preprocessed.gbif_citizen WHERE gbifID = c.gbifID),  -- index hit
+        (SELECT geom FROM preprocessed.gbif_expert WHERE gbifID = e.gbifID),  -- index hit
+        (SELECT geom FROM preprocessed.gbif_citizen WHERE gbifID = c.gbifID),  -- index hit
         0.1
     )
-WHERE ST_DWithin(e.geom, c.geom, 5000);   -- accurate metric distance
+WHERE ST_DWithin(e.geom, c.geom, 5000) AND e_month = c_month;
+
