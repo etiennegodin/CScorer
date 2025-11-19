70c5a85c6e8433d42e07cb96d2840017528d3a79
etienengodin <manat@DESKTOP-STSFAGO.localdomain>
2025-11-19 12:39:58 -0500

    connection cli to pipe wip

--- src/Cscorer/main.py
+++ src/Cscorer/main.py
@@ -1,19 +1,12 @@
 from .data.main import data_submodules
 from .features.main import features_submodules
-from .utils.debug import launch_debugger
 from .pipeline import Pipeline, PipelineModule, StepStatus
 from .pipeline.yaml_support import read_config
-
 import yaml
 from pathlib import Path
-import argparse
-import subprocess
-import asyncio
-
 from pprint import pprint
 import logging
 import shutil
-
 
 def pipe_reconstructor(pipeline:Pipeline):
     for module in pipeline.modules.values():
@@ -22,23 +15,8 @@ def pipe_reconstructor(pipeline:Pipeline):
             sub._parent = module
             for step in sub.steps.values():
                 step._parent = sub
-                
-def init_pipeline(args)->Pipeline:
-    
-    # Check if required file, else try dev mode
-    if not args.file:
-        if not args.dev:
-            raise UserWarning("Missing config file")
-        #dev branch
-        config_path = Path(__file__).parent.parent.parent / "work/dev/config.yaml"
-        #config_path = Path(__file__).parent.parent.parent / "work/pipe_test/config.yaml"
-
-    else:
-        config_path = args.file
-    
-    #Config as dict
-    config = read_config(config_path)
-    
+   
+def init_folder(config:dict, config_path:Path)->dict:
     folders = {}
     run_folder = Path(config_path).parent
     pipe_folder = (run_folder / 'pipeline')
@@ -65,34 +43,78 @@ def init_pipeline(args)->Pipeline:
     #Add to config dict
     config['folders'] = folders
     config['db_path'] = str(db_path)
+
+    return config  
+      
+def init_pipeline(args, pipe_struct:dict)->Pipeline:
     
+    # Check if required file, else try dev mode
+    if not args.file:
+        if not args.dev:
+            raise UserWarning("Missing config file")
+        #dev branch
+        #config_path = Path(__file__).parent.parent.parent / "work/dev/config.yaml"
+        config_path = Path(__file__).parent.parent.parent / "work/pipe_test/config.yaml"
+
+    else:
+        config_path = args.file
     
+    #Config as dict
+    config = read_config(config_path)
+    #Create folder structure
+    config = init_folder(config, config_path)
+    folders = config['folders']
+
     # Force flag to wipe data
     if args.force:
+        data_folder = Path(folders['data_folder'])
+        pipe_folder = Path(folders['pipe_folder'])
         if data_folder.exists():
             shutil.rmtree(str(data_folder))
         if pipe_folder.exists():
             shutil.rmtree(str(pipe_folder))
-        
+            
+        pipe = Pipeline(config = config)
+        pipe.logger.info("Forcing new instance from scratch")
+
     # Create folders 
     for folder in folders.values():
         Path(folder).mkdir(exist_ok= True)
 
-    #New instance if totally new run (or forced)
-    if not (pipe_folder/'pipe.yaml').exists() :
-        logging.info("No pipe data found, creating new instance from scratch")
-        # Create instance 
-        pipe = Pipeline(config = config)
-
     #Read from disk 
     else:
-        try:
-            #pipe = Pipeline.from_yaml_file(pipe_folder/'pipe.yaml')
-            pipe = yaml.load(open(pipe_folder/'pipe.yaml'), Loader=yaml.FullLoader)
-            pipe_reconstructor(pipe)
-            logging.info("Previous pipe data found, creating new instance from data on disk")
+        # New instance if totally new run 
+        if not (pipe_folder/'pipe.yaml').exists() :
+            # Create instance 
+            pipe = Pipeline(config = config)
+            pipe.logger.info("No pipe data found, creating new instance from scratch")
 
-        except Exception as e:
-            raise Exception(e)
+        else:
+            try:
+                #pipe = Pipeline.from_yaml_file(pipe_folder/'pipe.yaml')
+                pipe = yaml.load(open(pipe_folder/'pipe.yaml'), Loader=yaml.FullLoader)
+                pipe_reconstructor(pipe)
+                pipe.logger.info("Previous pipe data found, creating new instance from data on disk")
+
+            except Exception as e:
+                raise Exception(e)
+    
+    if args.full:
+        for module, submodules in pipe_struct.items():
+            for sub in submodules.keys():
+                pass
+        
+    quit()
+    #Add modules
+    data_module = PipelineModule('data',  func = data_submodules)
+    features_module = PipelineModule('features',  func = features_submodules)    
+    
+    pipe.add_module(data_module)
+    pipe.add_module(features_module)
+    
+    
+
+    pprint(pipe.modules)
+        
 
     return pipe
