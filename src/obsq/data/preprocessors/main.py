3f8c75996e8d0ab4647ca1b25b4b6a5cdd99de38
etienengodin <manat@DESKTOP-STSFAGO.localdomain>
2025-11-24 11:56:14 -0500

    inat api query build wip

--- src/obsq/data/preprocessors/main.py
+++ src/obsq/data/preprocessors/main.py
@@ -4,62 +4,65 @@ from ...utils.sql import read_sql_template, read_sql_file, simple_sql_query
 import duckdb
 import asyncio
 from pathlib import Path
+from ..loaders.factory import create_query
 
 async def data_preprocessors(pipe:Pipeline, submodule:PipelineSubmodule):
     create_schema(pipe.con, 'clean')
     create_schema(pipe.con, 'preprocessed')
 
     sql_folder = Path(__file__).parent
+    #Clean gbif occurrences
     clean_gbif_citizen = PipelineStep( "clean_gbif_citizen", func = clean_gbif_occurences)
     clean_gbif_expert = PipelineStep( "clean_gbif_expert", func = clean_gbif_occurences)
         
+    # Get additionnal expert users from inat
     extract_inat_expert = PipelineStep( "extract_inat_expert", func = simple_sql_query)
     
-    merge_inatOccurences = PipelineStep( "merge_inatOccurences", func = simple_sql_query)
-
+    #Remove expert occ from citizen, populate in expert_occ
     shuffle_inat_expert_obs = PipelineStep( "shuffle_inat_expert_obs", func = simple_sql_query)
-    matchGbifDatasets = PipelineStep( "matchGbifDatasets", func = simple_sql_query)
     
+    #Extract json response for users fields
     extract_observer_json = PipelineStep( "extract_observer_json", func = simple_sql_query)
+    
+    #Query iNat API with reduced gbif citizen occurences 
+    inatOccMetadata = PipelineStep("get_inatOccMetadata", func = get_inatOccMetadata)
+    
+    # Label citizen observations matching expert occurrences
+    matchGbifDatasets = PipelineStep( "matchGbifDatasets", func = simple_sql_query)
+
+    #merge_inatOccurences = PipelineStep( "merge_inatOccurences", func = simple_sql_query)
+
 
     
     submodule.add_step(clean_gbif_citizen)
     submodule.add_step(clean_gbif_expert)
     submodule.add_step(extract_inat_expert)
-    submodule.add_step(merge_inatOccurences)
+    #submodule.add_step(merge_inatOccurences)
     submodule.add_step(shuffle_inat_expert_obs)
     submodule.add_step(matchGbifDatasets)
     submodule.add_step(extract_observer_json)
+    submodule.add_step(inatOccMetadata)
 
     
     async with asyncio.TaskGroup() as tg:
         tg.create_task(submodule.steps['clean_gbif_citizen'].run(pipe, sql_folder = sql_folder))
         tg.create_task(submodule.steps['clean_gbif_expert'].run(pipe, sql_folder = sql_folder))
         
-    await merge_inatOccurences.run(pipe, sql_folder = sql_folder)
-    await extract_inat_expert.run(pipe, sql_folder = sql_folder)
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(extract_inat_expert.run(pipe, sql_folder = sql_folder))
+        tg.create_task(extract_observer_json.run(pipe, sql_folder = sql_folder))
+
     await shuffle_inat_expert_obs.run(pipe, sql_folder = sql_folder)
+    
+    
+    await inatOccMetadata.run(pipe)
+
     await matchGbifDatasets.run(pipe, sql_folder = sql_folder)
-    await extract_observer_json.run(pipe, sql_folder = sql_folder)
 
+    #await merge_inatOccurences.run(pipe, sql_folder = sql_folder)
 
-
-async def data_preprocessors_extract_observer_json(pipe:Pipeline, step:PipelineStep, sql_folder:Path):
-    con = pipe.con
-    table_name = "raw.inat_observers"
-    keys = con.execute(f"SELECT DISTINCT json_extract(json) FROM {table_name}").fetchone()[0]
-    
-    cols = ",\n".join(
-        f"json_extract(json_col, '$.{k}') AS {k}" for k in keys
-    )
-    
-    query = f""" CREATE OR REPLACE TABLE preprocessed.inat_observers AS
-                SELECT
-                    id,
-                    {cols}
-                FROM {table_name};
-                """
-
+async def get_inatOccMetadata(pipe:Pipeline, step:PipelineStep):
+    pass
     
 async def clean_gbif_occurences(pipe:Pipeline, step:PipelineStep, sql_folder:Path):
     con = pipe.con
