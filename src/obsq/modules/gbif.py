fd17e9564317906f5088b63a4b1bd9f6062da90d
etienengodin <manat@DESKTOP-STSFAGO.localdomain>
2025-11-25 22:55:13 -0500

    async + loaders

--- src/obsq/modules/gbif.py
+++ src/obsq/modules/gbif.py
@@ -1,40 +1,46 @@
-from obsq.pipeline import PipelineContext, Module, step
+from obsq.pipeline import PipelineContext, Module, SubModule, step
 from ..steps import GbifLoader
 from ..utils.duckdb import import_csv_to_db
 
 #Citizen data loader
-collect_citizen_data = GbifLoader('collect_citizen_data', predicates = {'BASIS_OF_RECORD': 'HUMAN_OBSERVATION'})
+collect_citizen_data = GbifLoader('collect_citizen_data')
+collect_expert_data = GbifLoader('collect_expert_data')
 
-collect_expert_data = GbifLoader('collect_expert_data', predicates={})
-
+sm_collect_all_gbif = SubModule("collect_all_gbif",[collect_citizen_data, collect_expert_data])
 
 @step 
-def create_expert_custom_predicates(context:PipelineContext):
+def create_custom_predicates(context:PipelineContext):
+    predicates = {}
+    #Prep citizen predicates
+    predicates['collect_citizen_data'] = {'BASIS_OF_RECORD': 'HUMAN_OBSERVATION'}
+    
     #Prep expert specific predicates
     datasets = context.config['gbif_datasets']
     dataset_keys = []
     for dataset in datasets.values():
         dataset_keys.append(dataset['key'])
-    predicates = {"DATASET_KEY" : dataset_keys }
+    predicates['collect_expert_data'] = {"DATASET_KEY" : dataset_keys }
+    
     return predicates
 
 @step
-def store_gbif_citizen_csv(context:PipelineContext):
+async def store_gbif_citizen_csv(context:PipelineContext):
     step_output = context.get_step_output("collect_citizen_data")
     if step_output is not None:
         file = step_output['output']
-        table = import_csv_to_db(context.con, file, "raw", "gbif_citizen", geo = True)
+        table =  import_csv_to_db(context.con, file, "raw", "gbif_citizen", geo = True)
         return table
+    
 @step
-def store_gbif_expert_csv(context:PipelineContext):
+async def store_gbif_expert_csv(context:PipelineContext):
     step_output = context.get_step_output("collect_expert_data")
     if step_output is not None:
         file = step_output['output']
         table = import_csv_to_db(context.con, file, "raw", "gbif_expert", geo = True)
         return table
 
+sm_store_all_gbif = SubModule("store_all_gbif",[store_gbif_citizen_csv, store_gbif_expert_csv])
 
-
-m_collect_gbif = Module("collect_gbif",[collect_citizen_data,store_gbif_citizen_csv,create_expert_custom_predicates])
+m_collect_gbif = Module("ingest_gbif",[create_custom_predicates, sm_collect_all_gbif, sm_store_all_gbif])
 
     
