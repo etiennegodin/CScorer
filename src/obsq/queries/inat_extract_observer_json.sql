1db44cd3f7c8988f0c92411d401345869ec5a64a
etiennegodin <etiennegodin@duck.com>
2025-12-01 00:57:18 -0500

    observers, need to filter

---
+++ src/obsq/queries/inat_extract_observer_json.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE VIEW clean.inat_observers AS
+
+WITH prep AS (
+  SELECT 
+  id_string,
+  
+  json_extract(json, '$.id') AS id,
+  json_extract_string(json, '$.login') AS login,
+  json_extract(json, '$.spam') AS spam,
+  json_extract(json, '$.suspended') AS suspended,
+  json_extract(json, '$.created_at') AS created_at,
+  json_extract_string(json, '$.login_autocomplete') AS login_autocomplete,
+  json_extract_string(json, '$.login_exact') AS login_exact,
+  json_extract_string(json, '$.name') AS "name",
+  json_extract_string(json, '$.name_autocomplete') AS name_autocomplete,
+  json_extract_string(json, '$.orcid') AS orcid,
+  json_extract(json, '$.icon') AS icon,
+  json_extract(json, '$.observations_count') AS observations_count,
+  json_extract(json, '$.identifications_count') AS identifications_count,
+  json_extract(json, '$.journal_posts_count') AS journal_posts_count,
+  json_extract(json, '$.activity_count') AS activity_count,
+  json_extract(json, '$.species_count') AS species_count,
+  json_extract(json, '$.universal_search_rank') AS universal_search_rank,
+  json_extract_string(json, '$.roles') AS roles,
+  json_extract(json, '$.site_id') AS site_id,
+  json_extract_string(json, '$.icon_url') AS icon_url
+
+FROM raw.inat_observers
+)
+
+-- remove non matching 
+SELECT * 
+FROM prep
+WHERE id_string = name
+;
