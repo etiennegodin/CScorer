88bbd98a36f7e0dbad5620ffb52fc8802a8f6801
etiennegodin <etiennegodin@duck.com>
2025-12-03 00:00:16 -0500

    phenology wip

--- src/obsq/data/main.py
+++
@@ -1,8 +0,0 @@
-from .loaders.main import data_loaders
-from .preprocessors.main import data_preprocessors
-
-from ..pipeline import Pipeline, PipelineModule, PipelineSubmodule, StepStatus
-import asyncio
-
-async def data_submodules(pipe:Pipeline, module:PipelineModule): 
-    pass
