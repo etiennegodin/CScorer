398f397fee277b7484bfa2cc78f58633ac9259c8
etiennegodin <etiennegodin@duck.com>
2025-12-02 23:15:12 -0500

    post inat cleanup for gbif

--- src/obsq/queries/gbif_clean_post_inat.sql
+++ src/obsq/queries/gbif_clean_post_inat.sql
@@ -1,6 +1,18 @@
-DELETE FROM preprocessed.gbif_citizen
-WHERE "recordedBy" IN(
+ALTER TABLE raw.discarded_inat_observers ADD COLUMN IF NOT EXISTS obs_count INTEGER;
+UPDATE raw.discarded_inat_observers t1
+SET obs_count = 
+(
+    SELECT COUNT(*) 
+    FROM clean.gbif_citizen_no_expert t2
+    WHERE t1.id_string = t2."recordedBy"
+);
+
+CREATE OR REPLACE TABLE preprocessed.gbif_citizen AS
+SELECT *
+FROM clean.gbif_citizen_no_expert
+WHERE "recordedBy" NOT IN(
                     SELECT id_string 
                     FROM raw.discarded_inat_observers 
-
 );
+
+
