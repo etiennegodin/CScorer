f66885b13c7e4d9baadab254656ba279265fc7b6
etiennegodin <etiennegodin@duck.com>
2025-12-04 11:09:25 -0500

    pipe restruct

--- src/obsq/modules/gbif/preprocess.py
+++ src/obsq/modules/gbif/preprocess.py
@@ -1,7 +1,7 @@
 from .clean import gbif_clean_submodule
 from ...pipeline import *
 from ...steps import *
-from ..inat.observations import preprocess_inat_observations
+from ..inat.observations import preprocess_inat_observations_submodule
 
 
 
@@ -13,24 +13,22 @@ extract_all_observers = SimpleQuery("extract_all_observers", query_name= "gbif_o
 #Get citizen experts 
 get_citizen_expert = SimpleQuery("get_citizen_expert", query_name= "gbif_observers_expert")
 
-get_citizen_observers = SimpleQuery("get_citizen_observers", query_name= "gbif_get_citizen_observers")
+get_citizen_observers = SimpleQuery("get_citizen_observers", query_name= "gbif_observers_citizen")
 
 # Send observations from citizen expert to expert table
-citizen_occ_to_expert = SimpleQuery("citizen_occ_to_expert", query_name= "gbif_citizen_occ_to_expert" )
+gbif_observers_obs_shuffle = SimpleQuery("gbif_observers_obs_shuffle", query_name= "gbif_observers_obs_shuffle" )
 
-filter_observers_sm = SubModule("filter_observers",[extract_all_observers,
+gbif_preprocess_observers_submodule = SubModule("gbif_preprocess_observers",[extract_all_observers,
                                                 get_citizen_expert,
                                                 get_citizen_observers,
-                                                citizen_occ_to_expert])
-
+                                                gbif_observers_obs_shuffle])
 
 # FULL MODULE 
 gbif_preprocess = Module("preprocess_gbif", [
                                            gbif_clean_submodule,
-                                           preprocess_inat_observations,
+                                           preprocess_inat_observations_submodule,
                                            gbif_citizen_join_inat_obs,
-                                            filter_observers_sm
-
+                                            gbif_preprocess_observers_submodule
                                            ])
 
     
--- src/obsq/queries/gbif_observers_obs_shuffle.sql
+++ src/obsq/queries/gbif_observers_obs_shuffle.sql
@@ -1,21 +1,21 @@
--- create gbif_citizen view
+-- create table from gbif_citizen_inat without expert observations
 CREATE OR REPLACE VIEW preprocess.gbif_citizen_no_expert AS
 SELECT *
 FROM preprocess.gbif_citizen_inat
 WHERE recordedBy NOT IN (SELECT recordedBy FROM observers.expert)
 ;
 
--- create gbif_expert_table to insert into new observations
+-- create base gbif_expert table (table instaed of view to insert into)
 CREATE OR REPLACE TABLE preprocessed.gbif_expert AS
 SELECT * FROM clean.gbif_expert;
 
 -- CREATE VIEW OF observations from citizen expert 
-CREATE OR REPLACE VIEW raw.citizen_occ_from_expert AS
+CREATE OR REPLACE VIEW preprocess.citizen_obs_from_expert AS
 SELECT *
-FROM clean.gbif_citizen
-WHERE recordedBy IN (SELECT recordedBy FROM raw.citizen_expert);
+FROM preprocess.gbif_citizen_inat
+WHERE recordedBy IN (SELECT recordedBy FROM observers.expert);
 
 -- add citizen_occ_from_expert VIEW TO TABLE
 INSERT INTO preprocessed.gbif_expert
-SELECT * FROM raw.citizen_occ_from_expert;
+SELECT * FROM preprocess.citizen_obs_from_expert;
 
