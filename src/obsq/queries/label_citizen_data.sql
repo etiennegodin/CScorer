be8e1db804524a099bfc1fb91eb01d4ef06bfe29
etiennegodin <etiennegodin@duck.com>
2025-12-05 15:09:44 -0500

    citizen label data

--- src/obsq/queries/label_citizen_data.sql
+++ src/obsq/queries/label_citizen_data.sql
@@ -1,26 +1,26 @@
 
 -- create spatial index for spatial join
-CREATE INDEX IF NOT EXISTS idx_c_geom  ON preprocessed.gbif_citizen USING RTREE (geom)
+CREATE INDEX IF NOT EXISTS idx_c_geom  ON preprocessed.gbif_citizen USING RTREE (geom);
 CREATE INDEX IF NOT EXISTS idx_e_geom  ON preprocessed.gbif_expert USING RTREE (geom);
 
 CREATE OR REPLACE TABLE labeled.gbif_citizen AS
 
 WITH c3857 AS(
     
-    SELECT gbifID, "month", taxonKey, ST_Transform(geom, 'EPSG:4326', 'EPSG:3857') AS geom
+    SELECT gbifID, "month", taxonID, ST_Transform(geom, 'EPSG:4326', 'EPSG:3857') AS geom
     FROM preprocessed.gbif_citizen
 ),
 
 e3857 AS(
     
-    SELECT gbifID, "month", taxonKey, ST_Transform(geom, 'EPSG:4326', 'EPSG:3857') AS geom
+    SELECT gbifID, "month", taxonID, ST_Transform(geom, 'EPSG:4326', 'EPSG:3857') AS geom
     FROM preprocessed.gbif_expert
 ),
 -- main label rules 
 matched_ids AS( 
 
-SELECT e.gbifID as e_id, e.month as e_month, e.taxonKey as e_taxon,
-    c.gbifID as c_id, c.month as c_month, c.taxonKey as c_taxon,
+SELECT e.gbifID as e_id, e.month as e_month, e.taxonID as e_taxon,
+    c.gbifID as c_id, c.month as c_month, c.taxonID as c_taxon,
 
 FROM e3857 e
 JOIN c3857 c
