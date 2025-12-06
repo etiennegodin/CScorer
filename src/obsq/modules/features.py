10557a0b708d47e7e911b253e09a0c679c0a84bc
etiennegodin <etiennegodin@duck.com>
2025-12-05 22:09:44 -0500

    combined features

--- src/obsq/modules/features.py
+++ src/obsq/modules/features.py
@@ -7,10 +7,13 @@ metadata_features = SimpleQuery('metadata_features', "features_metadata")
 phenology_features = SimpleQuery('phenology_features', "features_phenology")
 taxonomic_features = SimpleQuery('taxonomic_features', "features_taxonomic")
 histogram_features = SimpleQuery('histogram_features', "features_histogram")
+combined_features = SimpleQuery('combined_features', "features_combined")
+
 
 extractor_features = Module('extractor_features', [observer_feature,
                                                    community_validation_features,
                                                    metadata_features,
                                                    phenology_features,
                                                    taxonomic_features,
-                                                   histogram_features])
+                                                   histogram_features,
+                                                   combined_features])
--- src/obsq/queries/features_taxonomic.sql
+++ src/obsq/queries/features_taxonomic.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE VIEW features.taxonomic AS
 
 SELECT g.taxonID,
 
---COUNT(*) as count,
---ROUND(COUNT(g.taxonID) / SUM(COUNT(DISTINCT g.taxonID)) OVER (), 5) AS frequency,
---COUNT(s.similar_taxon) as similar_taxon
+COUNT(*) as count,
+ROUND(COUNT(g.taxonID) / SUM(COUNT(DISTINCT g.taxonID)) OVER (), 5) AS frequency,
+COUNT(s.similar_taxon) as similar_taxon
 FROM labeled.gbif_citizen g
 JOIN preprocessed.inat_similar_species s
     ON g."taxonID" = s."taxonID"
