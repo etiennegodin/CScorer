b35c872337dec9931121be2ae261453686095641
etiennegodin <etiennegodin@duck.com>
2025-12-07 21:28:10 -0500

    cleanup of nans for observer features

--- src/obsq/modules/features.py
+++ src/obsq/modules/features.py
@@ -4,6 +4,8 @@ from ..steps import *
 observer_feature = SimpleQuery('process_observers_features', "features_observers")
 community_validation_features = SimpleQuery('community_validation_features', "features_community_validation")
 metadata_features = SimpleQuery('metadata_features', "features_metadata")
+temporal_features = SimpleQuery('temporal_features', "features_temporal")
+
 phenology_features = SimpleQuery('phenology_features', "features_phenology")
 taxonomic_features = SimpleQuery('taxonomic_features', "features_taxonomic")
 histogram_features = SimpleQuery('histogram_features', "features_histogram")
@@ -15,5 +17,6 @@ extractor_features = Module('extractor_features', [observer_feature,
                                                    metadata_features,
                                                    phenology_features,
                                                    taxonomic_features,
+                                                   temporal_features,
                                                    histogram_features,
                                                    combined_features])
