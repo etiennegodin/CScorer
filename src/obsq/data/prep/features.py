a873f98bc85ac3104d5f1075a796965e95c7990e
etiennegodin <etiennegodin@duck.com>
2026-01-04 23:17:04 -0500

    pheno done

--- src/obsq/data/prep/features.py
+++ src/obsq/data/prep/features.py
@@ -6,17 +6,31 @@ community_validation_features = SimpleQuery('community_validation_features', "fe
 metadata_features = SimpleQuery('metadata_features', "features_metadata")
 temporal_features = SimpleQuery('temporal_features', "features_temporal")
 
-phenology_features = SimpleQuery('phenology_features', "features_phenology")
+features_phenology_leaves = SimpleQuery('features_phenology_leaves', "features_phenology_leaves")
+features_phenology_repro = SimpleQuery('features_phenology_repro', "features_phenology_repro")
+features_phenology_sex = SimpleQuery('features_phenology_sex', "features_phenology_sex")
+
 taxonomic_features = SimpleQuery('taxonomic_features', "features_taxonomic")
 histogram_features = SimpleQuery('histogram_features', "features_histogram")
 combined_features = SimpleQuery('combined_features', "features_combined")
 
 
+"""
 extractor_features_module = Module('extractor_features', [observer_feature,
                                                    community_validation_features,
                                                    metadata_features,
-                                                   
                                                    taxonomic_features,
                                                    temporal_features,
                                                    histogram_features,
+                                                   features_phenology_leaves,
+                                                   features_phenology_repro,
+                                                   features_phenology_sex,
+                                                   combined_features])
+"""
+
+extractor_features_module = Module('extractor_features', [observer_feature,
+                                                   community_validation_features,
+                                                   metadata_features,
+                                                   taxonomic_features,
+                                                   temporal_features,
                                                    combined_features])
