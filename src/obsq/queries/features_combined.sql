9664e6944d94e6d461624f803011ccb10d180ad7
etiennegodin <etiennegodin@duck.com>
2026-01-04 23:25:06 -0500

    combined

--- src/obsq/queries/features_combined.sql
+++ src/obsq/queries/features_combined.sql
@@ -8,23 +8,32 @@ SELECT g.gbifID,
     c.* EXCLUDE(c."gbifID"),
     a.* EXCLUDE(a."gbifID"),
     m.* EXCLUDE(m."gbifID"),
+    l.* EXCLUDE(l."gbifID"),
+    s.* EXCLUDE(s."gbifID"),
+    r.* EXCLUDE(r."gbifID"),
     t.* EXCLUDE(t."taxonID")
 
-FROM labeled.gbif_citizen g
-LEFT JOIN features.observer o
-    ON o."recordedBy" = g."recordedBy"
-LEFT JOIN features.taxonomic t
-    ON t."taxonID" = g."taxonID"
-JOIN features.community_validation c
-    ON c."gbifID" = g."gbifID"
-JOIN features.metadata m
-    ON m."gbifID" = g."gbifID"
-JOIN features.histogram h
-    ON h."gbifID" = g."gbifID"
-JOIN features.temporal a
-    ON a."gbifID" = g."gbifID";
 
-    
+FROM labeled.gbif_citizen g
+INNER JOIN features.observer o
+    ON o."recordedBy" = g."recordedBy"
+INNER JOIN features.taxonomic t
+    ON t."taxonID" = g."taxonID"
+INNER JOIN features.community_validation c
+    ON c."gbifID" = g."gbifID"
+INNER JOIN features.metadata m
+    ON m."gbifID" = g."gbifID"
+INNER JOIN features.histogram h
+    ON h."gbifID" = g."gbifID"
+INNER JOIN features.temporal a
+    ON a."gbifID" = g."gbifID"
+LEFT JOIN features.phenology_leaves l
+    on g."gbifID" = l."gbifID"
+LEFT JOIN features.phenology_sex s
+    on g."gbifID" = s."gbifID"
+LEFT JOIN features.phenology_repro r
+    on g."gbifID" = r."gbifID";
+
     ;
 
 
