158fe7174303533b9de57bfd8aa536032547aaaa
etiennegodin <etiennegodin@duck.com>
2026-01-04 19:54:11 -0500

    taxo features

--- src/obsq/queries/features_taxonomic.sql
+++ src/obsq/queries/features_taxonomic.sql
@@ -1,10 +1,19 @@
 CREATE OR REPLACE VIEW features.taxonomic AS
 
-SELECT g.taxonID,
 
-COUNT(*) as count,
-ROUND(COUNT(g.taxonID) / SUM(COUNT(DISTINCT g.taxonID)) OVER (), 5) AS frequency,
-COUNT(s.similar_taxon) as similar_taxon
+SELECT "taxonID",
+
+ROUND(COUNT(g.taxonID) /  SUM(COUNT(DISTINCT g.taxonID)) OVER (),3) as taxo_freq
+
+
+FROM labeled.gbif_citizen g
+GROUP BY g.taxonID
+;
+
+SELECT g.taxonID,
+ROUND(COUNT(DISTINCT g.taxonID) / SUM(COUNT(DISTINCT g.taxonID)) OVER (), 5) AS obsv_total_pct,
+ROUND(COUNT(g.taxonID) / SUM(COUNT(DISTINCT g.taxonID)) OVER (), 3) AS taxo_frequency,
+COUNT(s.similar_taxon) as taxo_similar_taxon
 FROM labeled.gbif_citizen g
 JOIN clean.inat_similar_species s
     ON g."taxonID" = s."taxonID"
