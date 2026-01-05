e13d0606eb7cd4c8c19330199204413590c4abee
etiennegodin <etiennegodin@duck.com>
2026-01-04 19:59:27 -0500

    histo features

--- src/obsq/queries/features_taxonomic.sql
+++ src/obsq/queries/features_taxonomic.sql
@@ -1,20 +1,16 @@
 CREATE OR REPLACE VIEW features.taxonomic AS
 
 
-SELECT "taxonID",
 
-ROUND(COUNT(g.taxonID) /  SUM(COUNT(DISTINCT g.taxonID)) OVER (),3) as taxo_freq
-
-
-FROM labeled.gbif_citizen g
-GROUP BY g.taxonID
-;
 
 SELECT g.taxonID,
-ROUND(COUNT(DISTINCT g.taxonID) / SUM(COUNT(DISTINCT g.taxonID)) OVER (), 5) AS obsv_total_pct,
-ROUND(COUNT(g.taxonID) / SUM(COUNT(DISTINCT g.taxonID)) OVER (), 3) AS taxo_frequency,
-COUNT(s.similar_taxon) as taxo_similar_taxon
+
+ROUND(COUNT(g.taxonID) /  SUM(COUNT(DISTINCT g.taxonID)) OVER (),3) as taxo_freq,
+(COUNT(DISTINCT s.similar_taxon)) as taxo_confusability_index
+
 FROM labeled.gbif_citizen g
 JOIN clean.inat_similar_species s
     ON g."taxonID" = s."taxonID"
 GROUP BY g.taxonID
+;
+
