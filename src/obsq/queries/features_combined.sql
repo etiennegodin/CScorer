d52bfa73747cb69a470b9b63c7645d22dacd7b02
etiennegodin <etiennegodin@duck.com>
2026-01-04 19:27:28 -0500

    model v1

--- src/obsq/queries/features_combined.sql
+++ src/obsq/queries/features_combined.sql
@@ -6,11 +6,14 @@ SELECT g.gbifID,
     h.* EXCLUDE(h."gbifID"),
     c.* EXCLUDE(c."gbifID"),
     a.* EXCLUDE(a."gbifID"),
-    m.* EXCLUDE(m."gbifID")
+    m.* EXCLUDE(m."gbifID"),
+    t.* EXCLUDE(t."taxonID")
 
 FROM labeled.gbif_citizen g
 LEFT JOIN features.observer o
     ON o."recordedBy" = g."recordedBy"
+LEFT JOIN features.taxonomic t
+    ON t."taxonID" = g."taxonID"
 JOIN features.community_validation c
     ON c."gbifID" = g."gbifID"
 JOIN features.metadata m
@@ -19,7 +22,7 @@ JOIN features.histogram h
     ON h."gbifID" = g."gbifID"
 JOIN features.temporal a
     ON a."gbifID" = g."gbifID";
-    
+
     
     ;
 
