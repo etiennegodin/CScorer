4777da7b0e2bde56b50eca6b9cc6600ccf418927
etiennegodin <etiennegodin@duck.com>
2026-01-07 18:02:58 -0500

    gee sampler

--- src/obsq/data/ingest/gee/main.py
+++ src/obsq/data/ingest/gee/main.py
@@ -1,20 +1,34 @@
 from ....pipeline import PipelineContext, step, Module
-from .core import GeeContext, GeePointSampler, wkt_string_to_geojson, gee_init
+from .core import GeeContext, GeePointSampler, wkt_string_to_geojson, init_gee
 import ee
+from pprint import pprint
 
 @step
-def gee_features_citizen(context:PipelineContext):
+async def gee_features_citizen(context:PipelineContext):
+    init_gee()
     config = context.config
     aoi_wkt = wkt_string_to_geojson(config['gbif_loader']['GEOMETRY'])
-    aoi = ee.Geometry.Polygon(aoi_wkt)
+    aoi = ee.Geometry.Polygon(aoi_wkt['coordinates'])
     points = ee.FeatureCollection('projects/observationscorer/assets/citizen_occurences')
-    context = GeeContext(aoi, points, config['time']['start'], config['time']['end'])
-                         
-    print(context)
+    gee_context = GeeContext(aoi, points, config['time']['start'], config['time']['end'])         
 
-    
+    sampler = GeePointSampler('citizen_occurences', gee_context)
+    return sampler._execute(context)
 
-gee_features_module = Module('gee_features',[gee_init, gee_features_citizen])
+
+@step
+async def gee_features_expert(context:PipelineContext):
+    init_gee()
+    config = context.config
+    aoi_wkt = wkt_string_to_geojson(config['gbif_loader']['GEOMETRY'])
+    aoi = ee.Geometry.Polygon(aoi_wkt['coordinates'])
+    points = ee.FeatureCollection('projects/observationscorer/assets/expert_occurences')
+    gee_context = GeeContext(aoi, points, config['time']['start'], config['time']['end'])         
+       
+    sampler = GeePointSampler('expert_occurences', gee_context)
+    return sampler._execute(context)
+
+gee_features_module = Module('gee_features',[gee_features_expert])
 
 
 
