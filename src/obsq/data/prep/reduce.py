79f2c2034d4d9f64ad0c923bd9adb19c0f05db3c
etiennegodin <etiennegodin@duck.com>
2026-01-11 00:09:41 -0500

    adding table_id

--- src/obsq/data/prep/reduce.py
+++ src/obsq/data/prep/reduce.py
@@ -6,6 +6,6 @@ import numpy as np
 
 ### Auto load all features and 
 
-obsv_reducer = Reducer(name = 'observer', )
+obsv_reducer = Reducer(name = 'observer', table_id='recordedBy' )
 
 reduce_features_module = Module('reduce_features', [obsv_reducer])
