9c89b0a130217d96adb9170ee3c1cf0393f27fb8
etiennegodin <etiennegodin@duck.com>
2026-01-13 18:17:30 -0500

    scorer wip

--- src/obsq/data/prep/label.py
+++ src/obsq/data/prep/label.py
@@ -8,8 +8,8 @@ label_citizen_data = SimpleQuery('label_citizen_data')
 
 
 spatial_match = SimpleQuery('score_spatial')
-expert_validation = SimpleQuery('')
+expert_validation = SimpleQuery('score_expert_validation')
 community_consensus = SimpleQuery('')
 phenology_match = SimpleQuery('')
 
-label_data_module = Module('label_data',[label_citizen_data, spatial_match])
+label_data_module = Module('score_observations',[spatial_match, expert_validation])
