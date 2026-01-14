ea09c7c97381c96b96c69b992421074710cc51be
etiennegodin <etiennegodin@duck.com>
2026-01-14 17:35:34 -0500

    split inat stats as other features, computed zscore for observations, id, species, total rank

---
+++ src/obsq/queries/features_obv_inat_stats.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE TABLE features.observer_inat AS
+
+WITH stats AS (
+    SELECT
+        AVG(LOG(1 + observations_count)) AS obv_mean_log,
+        STDDEV_POP(LOG(1 + observations_count)) AS obv_std_log,
+
+        AVG(LOG(1 + identifications_count)) AS id_mean_log,
+        STDDEV_POP(LOG(1 + identifications_count)) AS id_std_log,
+        
+        AVG(LOG(1 + species_count)) AS sp_mean_log,
+        STDDEV_POP(LOG(1 + species_count)) AS sp_std_log,
+
+        AVG(LOG(1 + universal_search_rank)) AS rank_mean_log,
+        STDDEV_POP(LOG(1 + universal_search_rank)) AS rank_std_log,
+
+    FROM observers.inat_data 
+)
+
+SELECT
+    c."recordedBy",
+    (LOG(1 + i.observations_count) - s.obv_mean_log)
+        / NULLIF(s.obv_std_log, 0) AS inat_obv_z_score,
+
+    (LOG(1 + i.identifications_count) - s.id_mean_log)
+        / NULLIF(s.id_std_log, 0) AS inat_id_z_score,
+
+    (LOG(1 + i.species_count) - s.sp_mean_log)
+        / NULLIF(s.sp_std_log, 0) AS inat_sp_z_score,
+
+    (LOG(1 + i.universal_search_rank) - s.rank_mean_log)
+        / NULLIF(s.rank_std_log, 0) AS inat_rank_z_score
+FROM observers.inat_data i
+JOIN observers.citizen c on i.user_id = c.user_id
+CROSS JOIN stats s;
