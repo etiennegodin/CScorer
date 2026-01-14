5c8c19a89942e3a614af0a2aaa6379c1e23e2f35
etiennegodin <etiennegodin@duck.com>
2026-01-13 19:17:17 -0500

    pheno coalesce to 0 if absent)

--- src/obsq/queries/features_phenology_sex.sql
+++ src/obsq/queries/features_phenology_sex.sql
@@ -2,20 +2,22 @@ CREATE OR REPLACE VIEW features.phenology_sex AS
 
 SELECT
     g.gbifID,
-    CASE g.month
-        WHEN 1 THEN p.month_1_density
-        WHEN 2 THEN p.month_2_density
-        WHEN 3 THEN p.month_3_density
-        WHEN 4 THEN p.month_4_density
-        WHEN 5 THEN p.month_5_density
-        WHEN 6 THEN p.month_6_density
-        WHEN 7 THEN p.month_7_density
-        WHEN 8 THEN p.month_8_density
-        WHEN 9 THEN p.month_9_density
-        WHEN 10 THEN p.month_10_density
-        WHEN 11 THEN p.month_11_density
-        WHEN 12 THEN p.month_12_density
-    END AS pheno_sex_month_density
+    COALESCE(
+        CASE g.month
+            WHEN 1 THEN p.month_1_density
+            WHEN 2 THEN p.month_2_density
+            WHEN 3 THEN p.month_3_density
+            WHEN 4 THEN p.month_4_density
+            WHEN 5 THEN p.month_5_density
+            WHEN 6 THEN p.month_6_density
+            WHEN 7 THEN p.month_7_density
+            WHEN 8 THEN p.month_8_density
+            WHEN 9 THEN p.month_9_density
+            WHEN 10 THEN p.month_10_density
+            WHEN 11 THEN p.month_11_density
+            WHEN 12 THEN p.month_12_density
+        END,
+        0.0) AS pheno_sex_month_density
 
 
 FROM preprocessed.gbif_citizen g
