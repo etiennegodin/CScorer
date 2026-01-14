e98c4e0b2df70f494dc0447a7fdc41703f86482e
etiennegodin <etiennegodin@duck.com>
2026-01-14 14:44:03 -0500

    left join multiplication

--- src/obsq/data/prep/score.py
+++ src/obsq/data/prep/score.py
@@ -6,7 +6,6 @@ from ...db import *
 
 label_citizen_data = SqlQuery('label_citizen_data')
 
-
 score_spatial = SqlQuery('score_spatial')
 score_expert_validation = SqlQuery('score_expert_validation')
 score_community = SqlQuery('score_community')
@@ -14,10 +13,10 @@ score_data_quality = SqlQuery('score_data_quality')
 score_scientific_value = SqlQuery('score_scientific_value')
 score_combined = SqlQuery('score_combined')
 
-
 score_total = SqlQuery('score_sum')
 
-#score_class = SqlQuery('score_class')
+score_class = SqlQuery('score_class')
 
+score_obs_module = Module('score_observations',[score_class])
 
-score_obs_module = Module('score_observations',[score_spatial, score_expert_validation, score_community, score_data_quality, score_scientific_value, score_combined])
+#score_obs_module = Module('score_observations',[score_spatial, score_expert_validation, score_community, score_data_quality, score_scientific_value, score_combined])
--- src/obsq/queries/score_class.sql
+++ src/obsq/queries/score_class.sql
@@ -44,20 +44,26 @@ observers AS (
     FROM observers.inat_data i
     JOIN observers.citizen c ON i.user_id = c.user_id
 
-)
+), 
+class5 AS(
 
-SELECT g.gbifID,
+SELECT g."gbifID",
+5 AS class
 
+FROM preprocessed.gbif_citizen g
+JOIN distance d ON g."gbifID" = d."gbifID"
+JOIN features.community_validation cv ON g.gbifID = cv.gbifID
+JOIN features.metadata m ON g.gbifID = m.gbifID
 
-CASE 
-    WHEN (d.month = g.month AND d.distance < 2000 AND cv.cmva_expert_id =1 AND m.meta_media_count >=2 AND m."meta_coordinateUncertaintyInMeters" < 300) THEN 5
-    WHEN (d.distance < 5000 AND h.histo_taxon_obs_month_density > 0.5 AND (cv.cmva_expert_id = 1 OR (cv.cmva_id_agree_rate = 1 AND cv.cmva_id_count >=3)) AND m.meta_media_count >=2) THEN 4
-    WHEN (r.range = 1 AND o.taxon_id_count > 5 AND cmva_id_count >= 3) THEN 3
-    WHEN (r.range =1 AND cv.cmva_id_agree_rate < 1.0 AND h.histo_taxon_obs_month_density < 0.5) THEN 2
-    WHEN (r.range = 0 ) THEN 1
-    ELSE 0
+WHERE d.month = g.month 
+AND d.distance < 2000 
+AND cv.cmva_expert_id =1 
+AND m.meta_media_count >=2 
+AND m."meta_coordinateUncertaintyInMeters" < 300
+), class4 AS(
 
-END AS "class"
+SELECT g."gbifID", 
+4 AS class
 
 FROM preprocessed.gbif_citizen g
 JOIN distance d ON g."gbifID" = d."gbifID"
@@ -67,5 +73,59 @@ JOIN features.phenology p ON g.gbifID = p.gbifID
 JOIN features.histogram h ON g.gbifID = h.gbifID
 JOIN features.ranges r ON g.gbifID = r.gbifID
 JOIN features.observers_taxon_id o ON g."identifiedBy" = g."identifiedBy"
+
+WHERE d.distance < 5000 
+AND h.histo_taxon_obs_month_density > 0.5 
+AND (cv.cmva_expert_id = 1 OR (cv.cmva_id_agree_rate = 1 AND cv.cmva_id_count >=3))
+AND m.meta_media_count >=2 
+), class3 AS(
+
+SELECT g."gbifID",
+3 AS class
+
+FROM preprocessed.gbif_citizen g
+JOIN features.community_validation cv ON g.gbifID = cv.gbifID
+JOIN features.ranges r ON g.gbifID = r.gbifID
+JOIN features.observers_taxon_id o ON g."identifiedBy" = g."identifiedBy"
+
+WHERE r.range = 1
+AND o.taxon_id_count > 5 
+AND cv.cmva_id_count >= 3 
+
+),class2 AS(
+
+SELECT g."gbifID",
+2 AS class
+
+FROM preprocessed.gbif_citizen g
+JOIN features.community_validation cv ON g.gbifID = cv.gbifID
+JOIN features.histogram h ON g.gbifID = h.gbifID
+JOIN features.ranges r ON g.gbifID = r.gbifID
+
+WHERE r.range =1 
+AND cv.cmva_id_agree_rate < 1.0 
+AND h.histo_taxon_obs_month_density < 0.5
+
+    
+), class1 AS(
+
+SELECT g."gbifID",
+1 AS class
+
+FROM preprocessed.gbif_citizen g
+JOIN features.ranges r ON g.gbifID = r.gbifID
+WHERE r.range = 1 
+)
+
+
+SELECT g.gbifID,
+COALESCE (c5.class, c4.class,c3.class,c2.class,c1.class, -1) AS class
+
+FROM preprocessed.gbif_citizen g
+LEFT JOIN class5 c5 ON g."gbifID" = c5."gbifID"
+LEFT JOIN class4 c4 ON g."gbifID" = c4."gbifID"
+LEFT JOIN class3 c3 ON g."gbifID" = c3."gbifID"
+LEFT JOIN class2 c2 ON g."gbifID" = c2."gbifID"
+LEFT JOIN class1 c1 ON g."gbifID" = c1."gbifID"
 
 
