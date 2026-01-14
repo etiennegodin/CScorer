3790d24215d21adeecb8292d3692c6337ea68148
etiennegodin <etiennegodin@duck.com>
2026-01-13 19:17:17 -0500

    pheno coalesce to 0 if absent)

--- src/obsq/queries/features_phenology_leaves.sql
+++ src/obsq/queries/features_phenology_leaves.sql
@@ -2,25 +2,28 @@ CREATE OR REPLACE VIEW features.phenology_leaves AS
 
 SELECT
     g.gbifID,
-    CASE g.month
-        WHEN 1 THEN p.month_1_density
-        WHEN 2 THEN p.month_2_density
-        WHEN 3 THEN p.month_3_density
-        WHEN 4 THEN p.month_4_density
-        WHEN 5 THEN p.month_5_density
-        WHEN 6 THEN p.month_6_density
-        WHEN 7 THEN p.month_7_density
-        WHEN 8 THEN p.month_8_density
-        WHEN 9 THEN p.month_9_density
-        WHEN 10 THEN p.month_10_density
-        WHEN 11 THEN p.month_11_density
-        WHEN 12 THEN p.month_12_density
-    END AS pheno_leaves_month_density
+    COALESCE(
+
+        CASE 
+            WHEN g.month = 1 THEN p.month_1_density
+            WHEN g.month = 2 THEN p.month_2_density
+            WHEN g.month = 3 THEN p.month_3_density
+            WHEN g.month = 4 THEN p.month_4_density
+            WHEN g.month = 5 THEN p.month_5_density
+            WHEN g.month = 6 THEN p.month_6_density
+            WHEN g.month = 7 THEN p.month_7_density
+            WHEN g.month = 8 THEN p.month_8_density
+            WHEN g.month = 9 THEN p.month_9_density
+            WHEN g.month = 10 THEN p.month_10_density
+            WHEN g.month = 11 THEN p.month_11_density
+            WHEN g.month = 12 THEN p.month_12_density
+        END, 
+        0.0) AS pheno_leaves_month_density
 
 
 FROM preprocessed.gbif_citizen g
 
-JOIN preprocessed.inat_phenology_pivoted p
+LEFT JOIN preprocessed.inat_phenology_pivoted p
     ON g."taxonID" = p."taxonID"
     AND p.value_id = g.pheno_leaves
 
