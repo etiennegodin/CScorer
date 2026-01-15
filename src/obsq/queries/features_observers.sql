294ed30569e8fb131e01655d20ff4b7fe47e4ad0
etiennegodin <etiennegodin@duck.com>
2026-01-15 15:43:35 -0500

    new species diversity metric for observer, removed repetitive metrics overlaping inat data

--- src/obsq/queries/features_observers.sql
+++ src/obsq/queries/features_observers.sql
@@ -1,26 +1,3 @@
-
--- obs count per species per obs
-CREATE OR REPLACE VIEW preprocessed.observer_species_stats AS (
-
-WITH species_count AS(
-
-SELECT
-COUNT(*) as species_obs,
-"recordedBy", "species"
-FROM preprocessed.gbif_citizen
-GROUP BY "recordedBy", species
-)
-
-SELECT
-recordedBy,
-ROUND(AVG(species_obs),2) as avg_species_obs_count,
-MAX(species_obs) as max_species_obs_count
-
-FROM species_count
-GROUP BY "recordedBy"
-);
-
--- main features
 CREATE OR REPLACE TABLE features.observer AS
 
 WITH yearly_observation AS(
@@ -42,32 +19,51 @@ year,
 month
 FROM preprocessed.gbif_citizen
 GROUP BY recordedBy, year, month
-) 
+),
+
+species_count AS(
+
+    SELECT "recordedBy", species, COUNT(*) as count
+    FROM preprocessed.gbif_citizen
+    GROUP BY "recordedBy", species
+),
+
+species_probability AS(
+SELECT s."recordedBy", s.species, s.count,
+SUM(s.count) OVER (PARTITION BY s."recordedBy") AS TotalCount,
+CAST(s.count AS DECIMAL(10, 4)) / SUM(s.count) OVER (PARTITION BY s."recordedBy") AS Probability
+FROM species_count s
+),
+
+species_entropy AS(
+SELECT p."recordedBy",
+ROUND(ABS(-SUM(p.Probability * LOG(2, p.Probability))),4)AS species_entropy -- LOG(2, value) for base-2 log
+FROM species_probability p
+GROUP BY p."recordedBy"
+)
 
 SELECT g.recordedBy,
+
+--taxonomic
+p.species_entropy as obsv_species_diversity,
 --counts
 COUNT(DISTINCT g.gbifID) as obsv_obs_count, 
-ROUND(COUNT(DISTINCT g.gbifID) / SUM(COUNT(DISTINCT "gbifID")) OVER (), 5) AS obsv_total_pct,
 -- ids
-COUNT(DISTINCT g.gbifID) FILTER (WHERE g.identifiedByID IS NOT NULL) AS obsv_expert_ids,
-ROUND(obsv_expert_ids / obsv_obs_count, 3) as obsv_expert_ids_pct,
+ROUND(COUNT(DISTINCT g.gbifID) FILTER (WHERE g.identifiedByID IS NOT NULL) /
+ obsv_obs_count, 3) as obsv_expert_ids_pct,
 ROUND(AVG(CAST("dateIdentified" AS DATE) - CAST("eventDate" AS DATE))) as obsv_avg_id_time,
---taxonomic
-COUNT(DISTINCT g.class) as obsv_class_count,
-COUNT(DISTINCT g."order") as obsv_order_count,
-COUNT(DISTINCT g.family) as obsv_family_count,
-COUNT(DISTINCT g.genus) as obsv_genus_count,
-COUNT(DISTINCT g.species) as obsv_species_count,
--- time
+
+--time
 COUNT(DISTINCT g."year") as obsv_unique_year_count, 
 COUNT(DISTINCT CAST (g.eventDate AS DATE)) as obsv_unique_dates,
-MAX(y.yearly_observations) as obsv_max_yearly_obs,
-MAX(m.monthly_observations) as obsv_max_monthly_obs,
-ROUND(AVG(y.yearly_observations),2) as obsv_avg_yearly_obs,
+obsv_obs_count / obsv_unique_year_count AS obs_per_year,
 ROUND(AVG(m.monthly_observations),2) as obsv_avg_monthly_obs,
---
-COUNT(DISTINCT g.coordinateUncertaintyInMeters ) FILTER (WHERE g.coordinateUncertaintyInMeters > 1000 ) as obsv_high_cood_un_obs, -- count obs with high uncer
-ROUND(obsv_high_cood_un_obs / obsv_obs_count, 3) as obsv_high_cood_un_pct,
+ROUND(AVG(y.yearly_observations),2) as obsv_avg_yearly_obs,
+
+-- pct obs with high uncer
+ROUND(COUNT(DISTINCT g.coordinateUncertaintyInMeters ) FILTER (WHERE g.coordinateUncertaintyInMeters > 1000 ) /
+ obsv_obs_count, 3) as obsv_high_cood_un_pct,
+
 ROUND(AVG(g.coordinateUncertaintyInMeters),3) as obsv_avg_coord_un,
 ROUND(AVG(g.media_count),2) as obsv_avg_media_count,
 ROUND(COUNT(DISTINCT g.pheno_sex ) FILTER ( WHERE g.pheno_sex IS NOT NULL )/obsv_obs_count, 3) AS obsv_sex_meta_pct,
@@ -78,21 +74,17 @@ CASE
     ELSE ROUND(AVG(LENGTH("occurrenceRemarks"))) 
 END AS obsv_avg_descr_len
 
-
 FROM preprocessed.gbif_citizen g
 JOIN yearly_observation y
     ON g.recordedBy = y.recordedBy
 JOIN monthly_observations m 
     ON g.recordedBy = m.recordedBy
+JOIN species_entropy p 
+    ON g."recordedBy" = p."recordedBy"
 
-GROUP BY g.recordedBy
+GROUP BY g.recordedBy,
+p.species_entropy
+ORDER BY g.recordedBy
 ;
 
---adding back species obs count
-ALTER TABLE features.observer ADD COLUMN IF NOT EXISTS obsv_avg_species_obs_count FLOAT;
-ALTER TABLE features.observer ADD COLUMN IF NOT EXISTS obsv_max_species_obs_count INT;
-UPDATE features.observer o
-SET obsv_avg_species_obs_count = s.avg_species_obs_count,
-    obsv_max_species_obs_count = s.max_species_obs_count
-FROM preprocessed.observer_species_stats s
-WHERE o.recordedBy = s.recordedBy;
+#ALTER TABLE features.observer DROP COLUMN IF EXISTS obsv_obs_count;
