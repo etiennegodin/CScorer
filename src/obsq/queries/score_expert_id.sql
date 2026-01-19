7ce135bce395609cf0d23a8b0622b2974ad02a62
etiennegodin <etiennegodin@duck.com>
2026-01-19 12:21:56 -0500

    model v1

--- src/obsq/queries/score_expert_id.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TABLE score.expert_id AS
-SELECT g.gbifID,
-
-CASE 
-    WHEN g.identifiedByID IS NOT NULL THEN 1
-    ELSE 0
-END AS expert_id_score
-
-FROM preprocessed.gbif_citizen g
--- src/obsq/queries/score_total.sql
+++ src/obsq/queries/score_total.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW score.main AS
 
 SELECT c.gbifID,
-(1 * s.spatial_score + 1 * p.pheno_score + 1 * o.observer_score + 1 *  i.identifier_score +  1 * e.expert_id_score ) as score,
+(1 * s.spatial_score + 1 * p.pheno_score + 1 * o.observer_score + 1 *  i.identifier_score +  1 * e.cmva_expert_id ) as score,
 c.* EXCLUDE(c.'gbifID')
 
 FROM features.combined_p c
@@ -9,5 +9,5 @@ JOIN score.spatial s ON c."gbifID" = s."gbifID"
 JOIN score.phenology p ON c."gbifID" = p."gbifID"
 JOIN score.observer o ON c."gbifID" = o."gbifID"
 JOIN score.identifier i ON c."gbifID" = i."gbifID"
-JOIN score.expert_id e ON c."gbifID" = e."gbifID"
+JOIN encoded.community_validation e ON c."gbifID" = e."gbifID"
 
