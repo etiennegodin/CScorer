2f10ec4fe3091e3e6fcca34cee843f63ad30384d
etiennegodin <etiennegodin@duck.com>
2026-01-18 22:26:19 -0500

    combine

--- src/obsq/data/prep/combine.py
+++ src/obsq/data/prep/combine.py
@@ -8,23 +8,21 @@ from .core import Combine
 
 combined_features = SqlQuery('combined_features', "features_combined")
 
-combined_features_p = Combine('combined_features_p', combine_dict = {'observer' : ('recordedBy', 'left')
-
+combined_features_p = Combine('combined_features_p', combine_dict = [('observer','recordedBy', 'inner'),
+                                                                     ('observer_inat','recordedBy','inner'),
+                                                                     ('taxonomic', 'taxonID','inner'),
+                                                                     ('community_validation', 'gbifID','inner'),
+                                                                     ('metadata','gbifID','inner'),
+                                                                     ('histogram','gbifID','inner'),
+                                                                     ('temporal','gbifID','inner'),
+                                                                     ('spatial','gbifID','inner'),
+                                                                     ('gee','gbifID','inner'),
+                                                                     ('ranges','gbifID','inner'),
+                                                                     ('phenology', 'gbifID','left')])
+                                                    
         
     
-                })
+
 
 combine_features_module = Module('combine_features', [combined_features, combined_features_p])
 
-
-x = {
-                    'observer_inat' : "recordedBy",
-                'taxonomic' : 'taxonID',
-                'community_validation' : 'gbifID',
-                'metadata' : 'gbifID',
-                'histogram' : 'gbifID',
-                'temporal' : 'gbifID',
-                'spatial':'gbifID',
-                'gee' :'gbifID',
-                'ranges' : 'gbifID'
-}
